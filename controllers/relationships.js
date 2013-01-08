11a1168c251546cd076d8636c5c9a0a78d687d31
Aaron Shafovaloff <aaronshaf@gmail.com>
2013-01-08 05:04:58 -0700

    Added more basic CRUD

--- controllers/relationships.js
+++ controllers/relationships.js
@@ -4,12 +4,42 @@ var Q = require("q");
 
 RelationshipsController = _.bindAll({
 	initialize: function(app) {
-		app.get('/relationships/:id', this.read);
+		console.log('RelationshipsController loaded');
+		app.get('/relationships', this.read);
+		app.get('/relationships/:_id', this.readOne);
+		app.post('/relationships', this.create);
+		app.post('/relationships/:_id', this.save);
+	},
+
+	create: function(req, res) {
+		Relationships.save(req.body).then(function (data) {
+			res.json(201,data);
+		}).fail(function() {
+			res.json(500,{error: "Could not create"});
+		});
+	},
+
+	save: function(req, res) {
+		Relationships.save(req.body).then(function (data) {
+			res.json(200,data);
+		}).fail(function() {
+			res.json(500,{error: "Could not create"});
+		});
 	},
 
 	read: function(req, res) {
-		res.json({
-			error: 'Not implemented yet.'
+		Relationships.find({}).then(function (data) {
+			res.json(data);
+		}).fail(function() {
+			res.json(500,{error: "Could not read"});
+		});
+	},
+
+	readOne: function(req, res) {
+		Relationships.findOne(req.params._id).then(function (data) {
+			res.json(data);
+		}).fail(function() {
+			res.json(500,{error: "Could not read"});
 		});
 	}
 });
